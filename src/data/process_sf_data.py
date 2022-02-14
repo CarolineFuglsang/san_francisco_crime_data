be9790120ed4169baff72bd299ae19955ed04add
Caroline Fuglsang-Damgaard <36299120+CarolineFuglsang@users.noreply.github.com>
2022-02-14 12:17:45 +0100

    processed data file

---
+++ src/data/process_sf_data.py
@@ -0,0 +1,19 @@
+# Process data set 
+import pandas as pd 
+
+# loading raw data 
+raw_sf = pd.read_csv("./data/raw/sf_data.csv", sep=';')
+raw_districts = pd.read_csv("./data/raw/sf_districts.csv", sep=';')
+
+joined_df = (raw_sf.set_index('id')
+    .join(raw_districts.set_index('id'), how = 'inner')
+    .reset_index()
+    .assign(date=lambda x: pd.to_datetime(x.date),
+            day=lambda x: x.date.dt.day,
+            month=lambda x: x.date.dt.month, 
+            year=lambda x: x.date.dt.year, 
+            hour=lambda x: [int(x.time[i][0:2]) for i in range(len(x.time))])
+    .drop(columns = ['description'])        
+    )
+    
+joined_df.to_csv("data/processed/sf_data_processed.csv", index = False)
