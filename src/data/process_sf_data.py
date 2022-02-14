3563f6b394278295a4b02ce844c0a62f9e028651
Caroline Fuglsang-Damgaard <36299120+CarolineFuglsang@users.noreply.github.com>
2022-02-14 18:01:21 +0100

    Update process_sf_data.py

--- src/data/process_sf_data.py
+++ src/data/process_sf_data.py
@@ -12,8 +12,10 @@ joined_df = (raw_sf.set_index('id')
             day=lambda x: x.date.dt.day,
             month=lambda x: x.date.dt.month, 
             year=lambda x: x.date.dt.year, 
-            hour=lambda x: [int(x.time[i][0:2]) for i in range(len(x.time))])
-    .drop(columns = ['description'])        
+            hour=lambda x: [int(x.time[i][0:2]) for i in range(len(x.time))],
+            is_weekend = lambda x: [x.weekday[i] in ["friday", "saturday","sunday"] for i in range(x.shape[0])])
+    .drop(columns = ['description', 'longitude', 'latitude'])        
     )
     
+
 joined_df.to_csv("data/processed/sf_data_processed.csv", index = False)
