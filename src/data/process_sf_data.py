0e2073c550623e4e3def9236929bfe315159ccc7
Caroline Fuglsang-Damgaard <36299120+CarolineFuglsang@users.noreply.github.com>
2022-02-15 15:57:16 +0100

    dataset for modelling task

--- src/data/process_sf_data.py
+++ src/data/process_sf_data.py
@@ -13,7 +13,8 @@ joined_df = (raw_sf.set_index('id')
             month=lambda x: x.date.dt.month, 
             year=lambda x: x.date.dt.year, 
             hour=lambda x: [int(x.time[i][0:2]) for i in range(len(x.time))],
-            is_weekend = lambda x: [x.weekday[i] in ["friday", "saturday","sunday"] for i in range(x.shape[0])])
+            is_weekend = lambda x: [x.weekday[i] in ["friday", "saturday","sunday"] for i in range(x.shape[0])], 
+            is_violent = lambda x: x.label == 'violent')
     .drop(columns = ['description', 'longitude', 'latitude'])        
     .query("date < '2018-05-15'")
     )    
@@ -21,8 +22,9 @@ joined_df = (raw_sf.set_index('id')
 joined_df.to_csv("data/processed/sf_data_processed.csv", index = False)
 
 
-# subsetting to a smaller dataset from 2013 to 2018
-(joined_df
-    .query("date >= '2013-01-01'")
-    .to_csv("data/processed/sf_data_processed_2013_2018.csv", index = False)
+# subsetting to a smaller dataset from 2013 to 2018 to do modelling on 
+model_data_set = (joined_df
+    .query("date >= '2013-01-01'")[["id", "weekday", "district", "day", "month", "year", "hour", "is_violent"]]
 )
+
+model_data_set.to_csv("data/processed/sf_data_processed_2013_2018.csv", index = False)
